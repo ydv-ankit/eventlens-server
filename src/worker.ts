4c2b4fc1e066cf726eb182e9568802eba967397d
Ankit Ydv <ankityadav11241@gmail.com>
2026-04-23 16:07:49 +0530

    feat: batch insertion into db

--- src/worker.ts
+++ src/worker.ts
@@ -3,6 +3,7 @@ import { EventRequestData } from "./types/api";
 import logger from "./utils/logger";
 
 export const asyncQueue = new Array<EventRequestData & {apiKey: string}>();
+const batchInsertionLimit = 100;
 
 ;(async function () {
     while (true){
@@ -10,31 +11,52 @@ export const asyncQueue = new Array<EventRequestData & {apiKey: string}>();
             await new Promise((r)=> setTimeout(r, 50));
             continue;
         }
-        const item = asyncQueue.shift();
         try {
-            logger.info("processing item: " + JSON.stringify(item));
-            if (item?.apiKey) {
-                const query = `
-                    SELECT id FROM analytics.project
-                    WHERE api_key = $1
-                `;
-                    
-                const q = await db.query(query, [item.apiKey]);
-                
-                if (q.rowCount === 1) {
-                    const projectId = q.rows[0].id;
-                    const insertEventQuery = `
-                        INSERT INTO analytics.raw_event (event_name, metadata, project_id, user_id, timestamp)
-                        VALUES ($1, $2, $3, $4, $5)
+            const insertionBatchValues = new Array();
+            for (let i = 0; i < batchInsertionLimit; i++) {
+                const item = asyncQueue.shift();
+                if (!item) break;   // no items in queue
+                logger.info("processing item: " + JSON.stringify(item));
+
+                if (item?.apiKey) {
+                    const query = `
+                        SELECT id FROM analytics.project
+                        WHERE api_key = $1
                     `;
-                    await db.query(insertEventQuery, [item?.event_name, item?.metadata, projectId, item?.user_id, item?.timestamp]);
-                    logger.info("processing completed for item: " + JSON.stringify(item));
-                    continue;
+                    
+                    const q = await db.query(query, [item.apiKey]);
+                    
+                    if (q.rowCount === 1) {
+                        const projectId = q.rows[0].id;
+                        const values = [item?.event_name, item?.metadata, projectId, item?.user_id, item?.timestamp];
+                        insertionBatchValues.push(...values);
+                    }
+                } else {
+                    logger.error("processing failed due to invalid apikey for item: " + JSON.stringify(item));
                 }
             }
-            logger.error("processing failed due to invalid apikey for item: " + JSON.stringify(item));
+            
+            let insertEventQuery = `
+                INSERT INTO analytics.raw_event (event_name, metadata, project_id, user_id, timestamp)
+                VALUES 
+            `;
+            const valuesLimit = Math.floor(insertionBatchValues.length / 5);
+            for (let i = 0; i < valuesLimit; i++){
+                insertEventQuery += `($${5 * i + 1}, $${5 * i + 2}, $${5 * i + 3}, $${5 * i + 4}, $${5 * i + 5})`;
+                if (i === valuesLimit - 1) {
+                    insertEventQuery += `;`;
+                }else {
+                    insertEventQuery += `,`;
+                }
+            }
+            logger.info("query: " + insertEventQuery);
+            logger.info("values: " + insertionBatchValues);
+
+            await db.query(insertEventQuery, insertionBatchValues);
         } catch (error) {
-            logger.error("failed to process queue item: " + JSON.stringify(item));
+            console.log(error);
+            
+            logger.error("failed to process queue items");
         }
     }
 })();
